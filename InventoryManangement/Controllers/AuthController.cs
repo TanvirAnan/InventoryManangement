6975b9e182fc812c64b8b6a166b86cedd1e05191
TanvirAnan <tanvirhassanananta23@gmail.com>
2025-04-01 05:12:30 +0600

    Add project files.

---
+++ InventoryManangement/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManangement.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            Console.WriteLine("username: " + username);
+            Console.WriteLine("password: " + password);        
+            ViewBag.username = username;
+            return View();
+        }
+    }
+}
